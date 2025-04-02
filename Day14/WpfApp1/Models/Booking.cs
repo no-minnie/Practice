0ad957cb597c6ee16e706b71d10c98422eca2db3
no-minnie <novikmaria32@gmail.com>
2025-04-02 15:43:52 +0300

    Добавлена папка Day14

---
+++ Day14/WpfApp1/Models/Booking.cs
@@ -0,0 +1,90 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApp1.Models
+{
+    public class Booking : INotifyPropertyChanged
+    {
+        private int _id;
+        private string _clientName = string.Empty;
+        private string _roomNumber = string.Empty;
+        private DateTime _startDate;
+        private DateTime _endDate;
+
+        public int Id
+        {
+            get => _id;
+            set => SetField(ref _id, value);
+        }
+
+        public string ClientName
+        {
+            get => _clientName;
+            set
+            {
+                if (SetField(ref _clientName, value))
+                {
+                    OnPropertyChanged(nameof(BookingSummary));
+                }
+            }
+        }
+
+        public string RoomNumber
+        {
+            get => _roomNumber;
+            set
+            {
+                if (SetField(ref _roomNumber, value))
+                {
+                    OnPropertyChanged(nameof(BookingSummary));
+                }
+            }
+        }
+
+        public DateTime StartDate
+        {
+            get => _startDate;
+            set
+            {
+                if (SetField(ref _startDate, value))
+                {
+                    OnPropertyChanged(nameof(BookingSummary));
+                }
+            }
+        }
+
+        public DateTime EndDate
+        {
+            get => _endDate;
+            set
+            {
+                if (SetField(ref _endDate, value))
+                {
+                    OnPropertyChanged(nameof(BookingSummary));
+                }
+            }
+        }
+
+        public string BookingSummary => $"Бронь №{Id}: {ClientName} (Комн.: {RoomNumber}) с {StartDate:dd.MM.yyyy} по {EndDate:dd.MM.yyyy}";
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return BookingSummary;
+        }
+    }
+}
---
+++ Day14/WpfApp1/View/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using WpfApp1.Models;
+
+namespace WpfApp1.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class RoomStatusToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is RoomStatus status)
+            {
+                switch (status)
+                {
+                    case RoomStatus.Available: return "Доступен";
+                    case RoomStatus.Occupied: return "Занят";
+                    case RoomStatus.Maintenance: return "На обслуживании";
+                    default: return status.ToString();
+                }
+            }
+            return string.Empty;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class RoomStatusToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is RoomStatus status)
+            {
+                switch (status)
+                {
+                    case RoomStatus.Available: return new SolidColorBrush(Colors.MediumSeaGreen);
+                    case RoomStatus.Occupied: return new SolidColorBrush(Colors.IndianRed);
+                    case RoomStatus.Maintenance: return new SolidColorBrush(Colors.SlateGray);
+                    default: return Brushes.Transparent;
+                }
+            }
+            return Brushes.Transparent;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
